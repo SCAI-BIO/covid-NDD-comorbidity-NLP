37f352f862f4e000ab49c307898d0848c4183956
Negin Sadat Babaiha <66859895+NeginBabaiha@users.noreply.github.com>
2025-07-18 09:37:38 +0200

    Update comorbidity-hypothesis-db.py

--- src/comorbidity-hypothesis-db.py
+++ src/comorbidity-hypothesis-db.py
@@ -1,9 +1,9 @@
 import webbrowser
 
 # Your Neo4j AuraDB connection details
-NEO4J_URI = "neo4j+s://09f8d4e9.databases.neo4j.io"
+NEO4J_URI = "neo4j+s://505f8b68.databases.neo4j.io"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "a8axV58SA-bajOWDieMaYFcf_U6NhG929g3atbsJQxg"
+NEO4J_PASSWORD = "P0jTUhN_UDCAPpxtjE61ZKkv8dOEf-6NjOiHAxoImPQ"
 
 # Construct the URL for Neo4j Browser with prefilled connection details
 neo4j_browser_url = f"https://browser.neo4j.io/?connectURL={NEO4J_URI}&username={NEO4J_USER}&password={NEO4J_PASSWORD}"
